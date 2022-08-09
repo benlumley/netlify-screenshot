c3f8aed146798d2148d37253c2c69cbd35206e89
Ben Lumley <ben@benlumley.co.uk>
2022-08-09 16:09:14 +0100

    roughed out PDF

--- functions/print/print.js
+++ functions/print/print.js
@@ -4,27 +4,86 @@ const qs = require("qs")
 const width = 1024
 const height = 1200
 
+const maxage = 60 * 60 * 24 * 7
+
 exports.handler = async (event, context) => {
   const path = event.path.replace("/.netlify/functions", "").replace("/print", "").replace(".pdf", "")
+    if (path.indexOf('favicon.ico') > -1) {
+        return {
+            statusCode: 404
+        }
+    }
+    event.queryStringParameters.takingss = 1;
   const url = `${process.env.BASE_URL}${path}${qs.stringify(event.queryStringParameters, { addQueryPrefix: true })}`
 
+    let args = chromium.args;
+    args.push(...[
+        '--autoplay-policy=user-gesture-required',
+        '--disable-background-networking',
+        '--disable-background-timer-throttling',
+        '--disable-backgrounding-occluded-windows',
+        '--disable-breakpad',
+        '--disable-client-side-phishing-detection',
+        '--disable-component-update',
+        '--disable-default-apps',
+        '--disable-dev-shm-usage',
+        '--disable-domain-reliability',
+        '--disable-extensions',
+        '--disable-features=AudioServiceOutOfProcess',
+        '--disable-hang-monitor',
+        '--disable-ipc-flooding-protection',
+        '--disable-notifications',
+        '--disable-offer-store-unmasked-wallet-cards',
+        '--disable-popup-blocking',
+        '--disable-print-preview',
+        '--disable-prompt-on-repost',
+        '--disable-renderer-backgrounding',
+        '--disable-setuid-sandbox',
+        '--disable-speech-api',
+        '--disable-sync',
+        '--hide-scrollbars',
+        '--ignore-gpu-blacklist',
+        '--metrics-recording-only',
+        '--mute-audio',
+        '--no-default-browser-check',
+        '--no-first-run',
+        '--no-pings',
+        '--no-sandbox',
+        '--no-zygote',
+        '--password-store=basic',
+        '--use-gl=swiftshader',
+        '--use-mock-keychain',
+    ]);
+
   const browser = await chromium.puppeteer.launch({
-    args: chromium.args,
+    args: args,
     defaultViewport: chromium.defaultViewport,
     executablePath: await chromium.executablePath,
-    headless: chromium.headless,
+    headless: true, // chromium.headless,
+    userDataDir: '/tmp'
   })
 
-  const page = await browser.newPage()
 
-  await page.on("dialog", async dialog => dialog.accept(" "))
+    console.log(1);
+    console.time('timer');
+    const page = await browser.defaultPage()
+    console.log(2);
+    console.timeLog('timer');
+    await page.setViewport({ width, height })
+    console.log(3);
+    console.timeLog('timer');
+    await page.goto(url)
+    console.log(4);
+    console.timeLog('timer');
+    await page.waitForSelector('#screenshotPdfFrame');
+    console.log(5);
+    console.timeLog('timer');
 
-  await page.setViewport({ width, height })
-
-  await page.goto(url, { waitUntil: "networkidle2" })
-
-  const pdf = await page.pdf({
+    page.emulateMediaType('screen');
+    const pdf = await page.pdf({
     format: "A4",
+    printBackground: true,
+    scale: 0.5,
     margin: {
       top: 20,
       right: 40,
@@ -33,12 +92,18 @@ exports.handler = async (event, context) => {
     },
   })
 
+  console.log(7);
+  console.timeEnd('timer');
+
+
   await browser.close()
 
   return {
     statusCode: 200,
     headers: {
       "Content-Type": "application/pdf",
+        "Content-Disposition": "attachment; filename=iiag.pdf",
+        "Cache-Control": `public, max-age=${maxage}`,
     },
     body: pdf.toString("base64"),
     isBase64Encoded: true,
