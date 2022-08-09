7ee421c5ff9b8cc1831df24c682456298168d63c
Ben Lumley <ben@benlumley.co.uk>
2022-08-09 13:11:40 +0100

    Add logging

--- functions/screenshot/screenshot.js
+++ functions/screenshot/screenshot.js
@@ -9,7 +9,7 @@ exports.handler = async (event, context) => {
   const path = event.path.replace("/.netlify/functions", "").replace("/screenshot", "").replace(".png", "")
   event.queryStringParameters.takingss = 1;
   const url = `${process.env.BASE_URL}${path}${qs.stringify(event.queryStringParameters, { addQueryPrefix: true })}`
-
+console.log(url);
   const browser = await chromium.puppeteer.launch({
     args: chromium.args,
     defaultViewport: chromium.defaultViewport,
@@ -17,17 +17,19 @@ exports.handler = async (event, context) => {
     headless: true // chromium.headless,
   })
 
+  console.log(1);
   const page = await browser.defaultPage()
-
+    console.log(2);
    await page.setViewport({ width, height })
-
+    console.log(3);
   await page.goto(url)
-
+    console.log(4);
   await page.waitForSelector('#screenshotPdfFrame');
-
+    console.log(5);
   const frame = await page.$('#screenshotPdfFrame');
+    console.log(6);
   const screenshot = await frame.screenshot()
-
+    console.log(7);
 //   const screenshot = await page.screenshot();
 
   await browser.close()
