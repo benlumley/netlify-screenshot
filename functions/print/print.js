42ed3b6da493399c5097418b6394841bfc24acc2
Ben Lumley <ben@benlumley.co.uk>
2024-10-09 11:40:15 +0100

    scale fiddle

--- functions/print/print.js
+++ functions/print/print.js
@@ -91,7 +91,7 @@ exports.handler = async (event, context) => {
     const pdf = await page.pdf({
     format: "A4",
     printBackground: true,
-    scale: 0.8,
+    scale: 0.7,
     landscape: true,
     margin: {
       top: 20,
